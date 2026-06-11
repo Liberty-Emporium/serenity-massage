b480ca453363e102c6460574768f535d3e9c3776
OWL Agent <owl@alexanderai.site>
2026-06-11 07:54:43 -0400

    Fix robots.txt and sitemap.xml serving via Django URL patterns

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,22 @@
 from django.urls import path
 from pages import views
+from django.views.generic import TemplateView
+from django.contrib.sitemaps.views import sitemap
+from django.contrib.sitemaps import Sitemap
+
+class StaticViewSitemap(Sitemap):
+    priority = 0.5
+    changefreq = 'weekly'
+
+    def items(self):
+        return ['home', 'about', 'services', 'red_light_therapy', 'reviews', 'contact', 'blog', 'booking']
+
+    def location(self, item):
+        return '/' + ('' if item == 'home' else item + '/')
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -11,4 +28,6 @@ urlpatterns = [
     path('blog/', views.blog, name='blog'),
     path('blog/<slug:slug>/', views.blog_post, name='blog_post'),
     path('booking/', views.booking, name='booking'),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
